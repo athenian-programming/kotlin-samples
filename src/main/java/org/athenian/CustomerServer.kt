d912b23ef8cb0c14e1929b3ba117c07ff37de869
Paul Ambrose <pambrose@mac.com>
2017-12-14 14:01:08 -0800

    Initial commit

--- src/main/java/org/athenian/CustomerServer.kt
+++ src/main/java/org/athenian/CustomerServer.kt
@@ -1,26 +1,31 @@
 package org.athenian
 
 import com.google.common.util.concurrent.AbstractIdleService
+import com.squareup.moshi.Moshi
 import spark.Service
+
+
+data class Customer(val id: Int, val name: String, val address: String, val paid: Boolean)
 
 class CustomerServer(val port: Int = 8080) : AbstractIdleService() {
 
     private val http = Service.ignite()
+    private val moshi = Moshi.Builder().build()
 
     override fun startUp() {
         this.http.port(this.port)
 
-        this.http.get("/") { req, res ->
+        this.http.get("/") { _, res ->
             res.type("text/plain")
             "This is the root"
         }
 
-        this.http.get("/plain-hello") { req, res ->
+        this.http.get("/plain-hello") { _, res ->
             res.type("text/plain")
             "Hello World!"
         }
 
-        this.http.get("/html-hello") { req, res ->
+        this.http.get("/html-hello") { _, res ->
             res.type("text/html")
             """
             <html>
@@ -32,6 +37,26 @@ class CustomerServer(val port: Int = 8080) : AbstractIdleService() {
             </html>
             """
         }
+
+        var customers = listOf<Customer>(Customer(1, "Bill Smith", "123 Main St", false),
+                                         Customer(2, "Jane Jackson", "1245 Birch Ave", true),
+                                         Customer(3, "Steve Stillwell", "433 Peach Lane", true),
+                                         Customer(4, "Mary McKenna", "3454 Apple St", false))
+
+        this.http.get("/customers") { _, res ->
+            res.type("application/json")
+            val jsonAdapter = this.moshi.adapter<Map<String, List<Customer>>>(Map::class.java)
+            jsonAdapter.toJson(mapOf("customers" to customers))
+        }
+
+        this.http.get("/customers/:id") { req, res ->
+            res.type("application/json")
+            val jsonAdapter = this.moshi.adapter<Map<String, Customer?>>(Map::class.java)
+            val id = req.params(":id").toInt()
+            val cust = customers.find { it.id == id }
+            jsonAdapter.toJson(mapOf("customer" to cust))
+        }
+
     }
 
     override fun shutDown() {
