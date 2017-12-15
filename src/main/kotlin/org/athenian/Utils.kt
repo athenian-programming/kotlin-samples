1d83fe0799d5a88526da411d437fbb45fa75a222
Paul Ambrose <pambrose@mac.com>
2017-12-15 14:06:02 -0800

    Initial commit

--- src/main/kotlin/org/athenian/Utils.kt
+++ src/main/kotlin/org/athenian/Utils.kt
@@ -2,14 +2,13 @@ package org.athenian
 
 import com.beust.jcommander.IParameterValidator
 import com.beust.jcommander.JCommander
-import java.lang.String.format
 
 fun getVersionDesc(asJson: Boolean): String {
     val annotation = CustomerServer::class.java.getPackage().getAnnotation(VersionAnnotation::class.java)
     return if (asJson)
-        format("{\"Version\": \"%s\", \"Release Date\": \"%s\"}", annotation.version, annotation.date)
+        "{\"Version\": \"$annotation.version\", \"Release Date\": \"$annotation.date\"}"
     else
-        format("Version: %s Release Date: %s", annotation.version, annotation.date)
+        "Version: $annotation.version Release Date: $annotation.date"
 }
 
 class VersionValidator : IParameterValidator {
